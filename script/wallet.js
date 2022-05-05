923d77d7b0f17f91d1d6f7ce7369b7a14f0ca554
Szymon3eK <sz.hunek.sh@gmail.com>
2022-05-05 22:03:51 +0200

    1.3v
    Ulepszanie portfela

---
+++ script/wallet.js
@@ -0,0 +1,9 @@
+function imagebtcclick() {
+    navigator.clipboard.writeText("bc1q5geutz32cxr80tpn9h92lulp45tf584nrskz67")
+    alert('Skopiowano tekst!')
+  }
+
+  function imageethclick() {
+    navigator.clipboard.writeText("0x665fCD50c1f620e6ad86BC91f386364f261DA8D4")
+    alert('Skopiowano tekst!')
+  }
